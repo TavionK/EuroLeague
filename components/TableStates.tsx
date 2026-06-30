f1fa0624a93ed01dc941db1479b9cf02397ec390
TavionK <tavion.k.britt99@gmail.com>
2026-06-29 23:17:39 -0400

    created the table states functions

---
+++ components/TableStates.tsx
@@ -0,0 +1,11 @@
+export function LoadingState() {
+  return <p>Loading...</p>;
+}
+
+export function ErrorState() {
+  return <p>Something went wrong, please try later</p>;
+}
+
+export function EmptyState() {
+  return <p>No players were found</p>;
+}
