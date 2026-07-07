d50700757f4083bb26d757a9b472b3baa83d1957
TavionK <tavion.k.britt99@gmail.com>
2026-07-07 11:03:30 -0400

    add nav to layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import type { Metadata } from "next";
 import "./globals.css";
 import QueryProvider from "@/components/QueryProvider";
 import Footer from "@/components/Footer";
+import Nav from "@/components/Nav";
 
 export const metadata: Metadata = {
   title: "EuroLeague Stats",
@@ -18,6 +19,7 @@ export default function RootLayout({
   return (
     <html lang="en" className={`h-full antialiased`}>
       <body className="flex min-h-full flex-col">
+        <Nav />
         <QueryProvider>{children}</QueryProvider>
         <Footer />
       </body>
